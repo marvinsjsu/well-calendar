6ce70f56422955b11a3110af9d6c474c479ba01c
Marvin Mante <marvinsjsu@gmail.com>
2019-08-02 22:57:34 -0700

    .

--- app/index.js
+++ app/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import Loading from './components/Loading';
 import Nav from './components/Nav';
-import MyAppointments from './components/MyAppointments';
-import RequestForm from './components/RequestForm';
 import NoMatch from './components/NoMatch';
 import { AppProvider } from './contexts/AppProvider';
 import { connected } from './utils/api';
 import { ROUTES } from './utils/constants';
 
 import './css/main.css';
+
+const RequestForm = React.lazy(() => import('./components/RequestForm'));
+const MyAppointments = React.lazy(() => import('./components/MyAppointments'));
 
 class App extends React.Component {
   render () {
@@ -19,11 +21,13 @@ class App extends React.Component {
         <AppProvider>
           <div className='container'>
             <Nav />
-            <Switch>
-              <Route exact path={ROUTES.HOME} component={RequestForm} />
-              <Route exact path={ROUTES.MY_APPOINTMENTS} component={MyAppointments} />
-              <Route component={NoMatch} />
-            </Switch>
+            <React.Suspense fallback={<Loading />}>
+              <Switch>
+                <Route exact path={ROUTES.HOME} component={RequestForm} />
+                <Route exact path={ROUTES.MY_APPOINTMENTS} component={MyAppointments} />
+                <Route component={NoMatch} />
+              </Switch>
+            </React.Suspense>
           </div>
         </AppProvider>
       </Router>
